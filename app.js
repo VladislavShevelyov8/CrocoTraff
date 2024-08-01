b6ed4185b5176c7cf97b1f67d4707a4ec24804da
Vladislav <97635122+VladislavShevelyov@users.noreply.github.com>
2024-08-01 17:17:16 +0200

    PreLoaded

---
+++ app.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function () {
+  var preloader = document.getElementById('preloader');
+  preloader.style.display = 'none';
+});
+
+
+let croco = document.getElementsByClassName("croco");
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	croco.classList.add('ready');
+});
