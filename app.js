655a251e85e522a2fc3696c50c2ecad09a676d23
Vladislav <97635122+VladislavShevelyov@users.noreply.github.com>
2024-08-01 20:43:42 +0200

    Finish

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-// // Loader
+// Loader
 
-// window.addEventListener('load', function () {
-//   var preloader = document.getElementById('preloader');
-//   preloader.style.display = 'none';
-// });
+window.addEventListener('load', function () {
+  var preloader = document.getElementById('preloader');
+  preloader.style.display = 'none';
+});
 
 
-// // CrocoLoad
+// CrocoLoad
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   let croco = document.getElementsByClassName("croco")[0];
-//   if (croco) {
-//       croco.classList.add('ready');
-//   }
-// });
+document.addEventListener('DOMContentLoaded', function() {
+  let croco = document.getElementsByClassName("croco")[0];
+  if (croco) {
+      croco.classList.add('ready');
+  }
+});
 
 
