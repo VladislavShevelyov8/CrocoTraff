561f511d668336ffed3129767847ca8d80fab782
Vladislav <97635122+VladislavShevelyov@users.noreply.github.com>
2024-08-02 18:16:49 +0200

    burger

--- app.js
+++ app.js
@@ -8,11 +8,30 @@ window.addEventListener('load', function () {
 
 // CrocoLoad
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   let croco = document.getElementsByClassName("croco")[0];
   if (croco) {
-      croco.classList.add('ready');
+    croco.classList.add('ready');
   }
 });
 
+
+// Burger
+
+
+function burger() {
+  let burgerSection = document.getElementsByClassName("burger__section")[0];
+  let burger = document.getElementsByClassName("burger")[0];
+  let area = document.getElementsByClassName("burger-area")[0];
+
+  burgerSection.classList.add("off");
+
+
+  if (burgerSection && burger && area) {
+    burger.classList.toggle("active");
+    burgerSection.classList.toggle("active");
+    area.classList.add("off");
+  }
+
+}
 
